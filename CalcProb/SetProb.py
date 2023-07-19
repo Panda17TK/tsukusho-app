db631cc8e9b511642f7a301676124154d2058861
Handa Taiki <bantian.tech@gmail.com>
2023-07-19 16:33:53 +0900

    debug path problem

--- CalcProb/SetProb.py
+++ CalcProb/SetProb.py
@@ -4,12 +4,12 @@ import pandas as pd
 from typing import List, Tuple, Dict, Any
 from ortoolpy import addvar, addvars, addbinvars
 import pandas as pd
-from pulp import LpProblem , LpMinimize , LpVariable , lpSum , PULP_CBC_CMD , LpStatus
+import pulp
 import time
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-from GetData import setdata
+from GetData.setdata import SetData
 
 class CalcProb(SetData):
 	def __init__(self) -> None:
