24ca87b64de2c992e466331b868137049e27132e
Handa Taiki <bantian.tech@gmail.com>
2023-07-20 22:47:41 +0900

    add model

--- CalcProb/SetProb.py
+++ CalcProb/SetProb.py
@@ -13,7 +13,7 @@ from GetData.setdata import SetData
 
 class CalcProb(SetData):
 	def __init__(self) -> None:
-		self.data_instance = setdata.SetData()
+		self.data_instance = SetData()
 		self.all_data:pd.DataFrame = self.data_instance.data_user_frame["全員"].replace("", pd.NA).set_index(["日付", "時間帯"]).dropna(how='all', axis=0)
 		self.user_list:list = list(self.all_data.columns)
 		self.time_list:list = list(self.all_data.index)
@@ -23,5 +23,9 @@ class CalcProb(SetData):
 		prob = pulp.LpProblem("PracticeTimeOptimization", pulp.LpMaximize)
 		# モデルの定義
 		self.x = pulp.LpVariable.dicts("x", [(i,j) for i in self.user_list for j in self.time_list], cat="Binary")
+		# x_ijは団員iが時間帯jに稽古に参加するかどうかを表す二値変数
+		x = pulp.LpVariable.dicts("x", [(i,j) for i in self.user_list for j in self.time_list], cat="Binary")
+		# 目的関数を設定する
+		prob += pulp.lpSum([x[(i,j)] for i in self.user_list for j in self.time_list])
 
 
